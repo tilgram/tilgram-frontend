280d8584f2548b49066d2851de445ef55dd9ab44
Omid Hezaveh <omid.hezaveh@futurice.com>
2017-10-21 20:13:01 +0300

    Set api_root for production

--- src/agent.js
+++ src/agent.js
@@ -3,8 +3,11 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-//const API_ROOT = 'https://conduit.productionready.io/api';
-const API_ROOT = 'http://localhost:3000/api';
+let API_ROOT = 'http://localhost:3000/api';
+
+if (process.env.NODE_ENV === 'production') {
+  API_ROOT = '/api';
+}
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
